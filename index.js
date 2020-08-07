80ea762c9061ea1d1e8c4b4a76e72bd9574435e5
Quang Nhat <doanquangnhat97@gmail.com>
2020-08-08 00:43:44 +0700

    add users and style for home

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=> {
-	res.send('Hello');
+	res.send('<h1>Hello</h1>');
 });
+
+app.get('/users', (req ,res) =>{
+	res.send("users")
+})
 
 app.listen(port, () => console.log(`I'm listening on port ${port}`));
