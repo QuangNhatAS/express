e7e1ccc6149d229440ca5a6b060d04d08e97cd0a
Quang Nhat <doanquangnhat97@gmail.com>
2020-08-11 11:40:04 +0700

    query parameters

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=> {
-	res.send('<h2>Hello</h2>');
+const users = [
+	{ id: 1, name: "Quang"},
+	{ id: 2, name: "Nhat"}
+]
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		name: "Nhat"
+	})
+});
+app.get('/users', (req, res) => {
+	res.render('users/index', {
+		users: users
+	})
 });
 
-app.get('/users', (req ,res) =>{
-	res.send("users")
+app.get('/users/search', (req, res) => {
+	const q = req.query.q.toLowerCase();
+	const matchUser = users.filter(user => user.name.toLowerCase().indexOf(q) !== -1)
+	res.render('users/index', {
+		users: matchUser
+	})
 })
 
-app.listen(port, () => console.log(`I'm listening on port ${port}`));
-
+app.listen(port, () => console.log(`I am listening on ${port}`))
