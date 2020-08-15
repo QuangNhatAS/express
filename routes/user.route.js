bbda878baae1077250aa14b42f7262b096e2f84e
Quang Nhat <doanquangnhat97@gmail.com>
2020-08-15 10:26:13 +0700

    using middleware to validate

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 const controller = require('../controllers/user.controller');
 
+const validate = require('../validate/user.validate');
+
 router.get('/', controller.home);
 
 router.get('/search', controller.search);
@@ -12,6 +14,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.view)
 
-router.post('/create', controller.postCreate)
+router.post('/create',validate.postCreate, controller.postCreate)
 
 module.exports = router;
